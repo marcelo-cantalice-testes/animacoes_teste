d83f8df42c91fbe283cbfbf11e2afd5c4bb28419
marcelo-cantalice-testes <katakumbio@gmail.com>
2022-01-06 16:34:54 -0300

    06Janeiro2022

--- lib/TesteAnimacao.dart
+++ lib/TesteAnimacao.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+// ignore_for_file: avoid_print, prefer_const_constructors, file_names, unused_field, prefer_final_fields
 
+import 'package:flutter/material.dart';
+//Testando
 class TesteAnimacao extends StatefulWidget {
   const TesteAnimacao({ Key? key }) : super(key: key);
 
@@ -8,22 +10,29 @@ class TesteAnimacao extends StatefulWidget {
 }
 
 class _TesteAnimacaoState extends State<TesteAnimacao> {
+  bool _status= true;
   @override
   Widget build(BuildContext context) {
     return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
+          AnimatedContainer(
             padding: EdgeInsets.all(20),
-            width: 300,
-            height: 300,
-            color: Colors.purpleAccent,
+            width: _status ? 100 : 500,
+            height: _status ? 300 : 200,
+            color: _status ? Colors.purpleAccent : Colors.red,
             child: Image.asset('images/logo.png'),
+            duration: Duration(seconds: 2),
+            
+            
           ),
           ElevatedButton(
               onPressed: () {
-                print("Botao clciado");
-                setState(() {});
+                
+                setState(() {
+                   _status=!_status;
+                });
+                print("Status: "+_status.toString());
               },
               child: Text("Alterar"))
         ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 import 'TesteAnimacao.dart';
 
 void main() {
+  // ignore: prefer_const_constructors
   runApp(MaterialApp(
     home: TesteAnimacao(),
     debugShowCheckedModeBanner: false,
