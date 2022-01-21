df5f24dda7d9e38d2c1efabd39bf95dff5817dc1
marcelo-cantalice-testes <katakumbio@gmail.com>
2022-01-21 09:02:44 -0300

    AnimacoesTween

--- lib/TesteAnimacao.dart
+++ lib/TesteAnimacao.dart
@@ -1,41 +1,40 @@
 // ignore_for_file: avoid_print, prefer_const_constructors, file_names, unused_field, prefer_final_fields
 
 import 'package:flutter/material.dart';
+
 //Testando
 class TesteAnimacao extends StatefulWidget {
-  const TesteAnimacao({ Key? key }) : super(key: key);
+  const TesteAnimacao({Key? key}) : super(key: key);
 
   @override
   _TesteAnimacaoState createState() => _TesteAnimacaoState();
 }
 
 class _TesteAnimacaoState extends State<TesteAnimacao> {
-  bool _status= true;
+  bool _status = true;
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          AnimatedContainer(
-            padding: EdgeInsets.all(20),
-            width: _status ? 100 : 500,
-            height: _status ? 300 : 200,
-            color: _status ? Colors.purpleAccent : Colors.red,
-            child: Image.asset('images/logo.png'),
-            duration: Duration(seconds: 2),
-            
-            
-          ),
-          ElevatedButton(
-              onPressed: () {
-                
-                setState(() {
-                   _status=!_status;
-                });
-                print("Status: "+_status.toString());
-              },
-              child: Text("Alterar"))
-        ],
-      );
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        AnimatedContainer(
+          padding: EdgeInsets.all(20),
+          width: _status ? 100 : 500,
+          height: _status ? 300 : 200,
+          color: _status ? Colors.purpleAccent : Colors.red,
+          child: Image.asset('images/logo.png'),
+          duration: Duration(seconds: 2),
+          curve: Curves.bounceIn,
+        ),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _status = !_status;
+              });
+              print("Status: " + _status.toString());
+            },
+            child: Text("Alterar"))
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:animacoes_teste/CriandoAnimacoesBasicas.dart';
 import 'package:flutter/material.dart';
 
+import 'AnimacaoTween.dart';
 import 'TesteAnimacao.dart';
 
 void main() {
   // ignore: prefer_const_constructors
   runApp(MaterialApp(
-    home: TesteAnimacao(),
+    home: AnimacaoTweenState(),
     debugShowCheckedModeBanner: false,
   ));
 }
